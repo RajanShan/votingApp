5afc2a28a4f60a3378fcaa67d3efd369615503d8
unknown <madhusudanmalik1795@gmail.com>
2024-05-20 15:02:01 +0530

    updated PORT using dotenv module

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const candidateRoutes = require('./routes/candidateRoute');
 const {jwtAuthMiddleware,generateToken} = require('./jwt');
 require('dotenv').config();
 
+const PORT = prcoess.env.PORT || 3000;
 const bodyparser = require('body-parser')
 app.use(bodyparser.json());
 
 
 app.use('/users',userRoutes);
 app.use('/candidate',candidateRoutes);
-
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log("Server is listening at 3000");
 })
